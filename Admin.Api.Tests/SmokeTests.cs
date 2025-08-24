38b0326f8e04385a43a97db12377f6f73657bad2
Jason Cavalere <jasoncavaliere@users.noreply.github.com>
2025-08-24 12:07:11 -0500

    test project setup

---
+++ Admin.Api.Tests/SmokeTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Admin.Api.Tests
+{
+    public class SmokeTests
+    {
+        [Fact]
+        public void TestProjectSetup()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Admin.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Admin.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
